4b629611defe85246c7f713c553921712e93a9dc
jahid-sourav <info.jahidsourav@gmail.com>
2022-09-25 16:47:51 +0600

    Binding Text Practice Project Added

---
+++ binding-text/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1 v-text="title"></h1>
+  <p>{{ greet }} {{ name }}</p>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      title : 'Practice for Binding Text',
+      name : 'Jahid',
+      greet : 'Hello',
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
